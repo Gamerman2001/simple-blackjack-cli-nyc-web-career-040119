82e3f4d2979516047932d473aeff4c942462cc43
Troy Gooden <tro12185@gmail.com>
2019-03-19 02:32:09 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,73 +1,73 @@
 def welcome
-  # code #welcome here
-  puts "Welcome to the Blackjack Table"
-end
-
-def deal_card
-  # code #deal_card here
-  rand(1..11)
-end
-
-def display_card_total(card_total)
-  # code #display_card_total here
-  puts "Your cards add up to #{card_total}"
-end
-
-def prompt_user
-  # code #prompt_user here
-  puts "Type 'h' to hit or 's' to stay"
-end
-
-def get_user_input
-  # code #get_user_input here
-  gets.chomp
-end
-
-def end_game(card_total)
-  # code #end_game here
-  puts "Sorry, you hit #{card_total}. Thanks for playing!"
-end
-
-def initial_round
-  # code #initial_round here
- card_total = 0 
- card_total += deal_card
- card_total += deal_card 
- display_card_total(card_total)
-   card_total
-end
-
-def hit?(card_total)
-
-    prompt_user
-    input = get_user_input
-    if input == "s"
-      hit?(card_total)
-    elsif input == "h"
-      card_total += deal_card
-    else invalid_command
+    # code #welcome here
+    puts "Welcome to the Blackjack Table"
+  end
+  
+  def deal_card
+    # code #deal_card here
+    rand(1..11)
+  end
+  
+  def display_card_total(card_total)
+    # code #display_card_total here
+    puts "Your cards add up to #{card_total}"
+  end
+  
+  def prompt_user
+    # code #prompt_user here
+    puts "Type 'h' to hit or 's' to stay"
+  end
+  
+  def get_user_input
+    # code #get_user_input here
+    gets.chomp
+  end
+  
+  def end_game(card_total)
+    # code #end_game here
+    puts "Sorry, you hit #{card_total}. Thanks for playing!"
+  end
+  
+  def initial_round
+    # code #initial_round here
+   card_total = 0 
+   card_total += deal_card
+   card_total += deal_card 
+   display_card_total(card_total)
+     card_total
+  end
+  
+  def hit?(card_total)
+    # code hit? here
       prompt_user
+      input = get_user_input
+      if input == "s"
+        hit?(card_total)
+      elsif input == "h"
+        card_total += deal_card
+      else invalid_command
+        hit?(card_total)
+      end 
+        card_total
+  end
+  
+  def invalid_command
+    # code invalid_command here
+    puts "please enter a valid command"
+  end
+  
+  #####################################################
+  # get every test to pass before coding runner below #
+  #####################################################
+  
+  def runner
+    welcome
+    total = initial_round
+    until total > 21
+    hit?(total)
+     total += deal_card
+     display_card_total(total)
     end 
-      card_total
-end
-
-def invalid_command
-  # code invalid_command here
-  puts "please enter a valid command"
-end
-
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
-
-def runner
-  welcome
-  total = initial_round
-  until total > 21
-  hit?(total)
-   total += deal_card
-   display_card_total(total)
-  end 
-end_game(total)
-end
+  end_game(total)
+  end
     
